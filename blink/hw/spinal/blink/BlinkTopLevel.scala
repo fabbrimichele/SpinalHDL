5662c4c544c9de0eeaf9ad7cce0aefab8e12a41a
Michele Fabbri <fabbrimichele75@gmail.com>
2025-06-22 19:10:58 +0200

    Using computing shield

--- blink/hw/spinal/blink/BlinkTopLevel.scala
+++ blink/hw/spinal/blink/BlinkTopLevel.scala
@@ -5,8 +5,10 @@ import spinal.core._
 // Hardware definition
 case class BlinkTopLevel() extends Component {
     val io = new Bundle {
-        val clk = in Bool()
-        val led = out Bool()
+        val led1 = out Bool()
+        val led2 = out Bool()
+        val led3 = out Bool()
+        val led4 = out Bool()
     }
 
     val ledReg =  Reg(Bool()) init(False)
@@ -18,7 +20,13 @@ case class BlinkTopLevel() extends Component {
         ledReg := ~ ledReg
     }
 
-    io.led := ledReg
+    io.led1 := ledReg
+    io.led2 := True
+    io.led3 := False
+    io.led4 := True
+
+    // Remove io_ prefix
+    noIoPrefix()
 }
 
 object BlinkTopLevelVerilog extends App {
